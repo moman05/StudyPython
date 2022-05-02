8ccc0b5c9a2f2c6316eef4bc7c9ac54095cfd57f
moman05 <gyej3858@gmail.com>
2022-05-02 11:49:35 +0900

    220502 파이썬 기본 문법학습

---
+++ basic/datatype02.py
@@ -0,0 +1,11 @@
+# 문자열
+bruce_eckel = 'Life is short. \n\tYou need Python.'
+print(bruce_eckel)
+
+#여러줄 문자열
+multi_line = '''Likfe is short.
+You need Python.
+And I need C#, too.
+'''
+print(multi_line)
+
